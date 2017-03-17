49f108f96616b0453ba338aab8d4f7ec4d670496
geekplux <geekplux@qq.com>
2017-03-18 01:52:12 +0800

    first blood

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center">template-nm</h1>
+
+<p align="center">
+  Scaffolding out a node module.
+</p>
+
+<img src="./media/preview.png" alt="preview">
+
+## Features
+
+- Unit test with [jest](https://facebook.github.io/jest/)
+- ESLint with [xo](https://github.com/sindresorhus/xo) or [standard](https://github.com/feross/standard)
+- CircleCI with [Yarn](yarnpkg.com) support
+- Optionally compile ES2015 code using [bili](https://github.com/unipahq/bili)
+- Optionally add coverage report
+
+## Usage
+
+Install [SAO](https://github.com/egoist/sao) first.
+
+### From git
+
+```bash
+sao egoist/template-nm
+```
+
+### From npm
+
+```bash
+sao nm
+```
+
+## License
+
+MIT &copy; EGOIST
---
+++ sao.js
@@ -0,0 +1,81 @@
+const superb = require('superb')
+const camelcase = require('camelcase')
+
+module.exports = {
+  templateOptions: {
+    context: {
+      camelcase
+    }
+  },
+  prompts: {
+    name: {
+      message: 'What is the name of the new project?',
+      role: 'folder:name'
+    },
+    description: {
+      message: 'How would you descripe the new project?',
+      default: `my ${superb()} project`
+    },
+    username: {
+      message: 'What is your GitHub username?',
+      role: 'git:name',
+      store: true
+    },
+    email: {
+      message: 'What is your GitHub email?',
+      role: 'git:email',
+      store: true
+    },
+    website: {
+      message: 'What is the url of your website?',
+      default(answers) {
+        return `https://github.com/${answers.username}`
+      },
+      store: true
+    },
+    unitTest: {
+      message: 'Do you need unit test?',
+      type: 'confirm'
+    },
+    coverage: {
+      message: 'Do you want to add coverage port?',
+      type: 'confirm',
+      default: false,
+      when: answers => answers.unitTest
+    },
+    eslint: {
+      message: 'Choose a eslint tool',
+      type: 'list',
+      choices: [
+        'xo',
+        'standard',
+        'disable'
+      ]
+    },
+    compile: {
+      message: 'Do you need to compile ES2015 code?',
+      type: 'confirm',
+      default: false
+    },
+    twitter: {
+      message: 'What is your twitter username?',
+      store: true
+    }
+  },
+  filters: {
+    'test/**': 'unitTest',
+    'src/**': 'compile',
+    'index.js': '!compile'
+  },
+  move: {
+    'gitignore': '.gitignore'
+  },
+  post({chalk, isNewFolder, folderName, log, install, init}) {
+    install()
+    init()
+    log.success('Done, let the hacking begin!')
+    if (isNewFolder) {
+      console.log(`  cd ${chalk.yellow(folderName)} to get started!\n`)
+    }
+  }
+}
